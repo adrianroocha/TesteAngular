e75e5191796a5e1db2a2d6c6956ad5d15fef195d
adrianroocha <adrianroocha@gmail.com>
2022-01-17 20:48:57 -0300

    Modulo course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
